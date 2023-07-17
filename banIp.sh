17622ba248dba0a439ec99d71f306188d9e82da3
jingangzhou <jingang.zhou@ai-endoangel.com>
2023-07-17 14:26:32 +0800

    update: use ALL, and iptables

--- banIp.sh
+++ banIp.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
-cat /var/log/auth.log | awk '/Invalid/{print $(NF-2)}' | sort | uniq -c | awk '{print $2"="$1}' > /tmp/black 
-list=`cat /tmp/black`
+logPath="/var/log/auth.log"
+tmpPath="/tmp/black"
+cat $logPath | awk '/Invalid/{print $(NF-2)}' | sort | uniq -c | awk '{print $2"="$1}' >>$tmpPath
+cat $logPath | awk '/Failed/{print $(NF-3)}' | sort | uniq -c | awk '{print $2"="$1;}' >>$tmpPath
+cat $logPath | awk '/Connection closed/{print $( NF-3)}' | sort | uniq -c | awk '{print $2"="$1;
+}' >>$tmpPath
+
+list=$(cat /tmp/black)
 #cat /tmp/black >> /opt/scripts/banIp.log
 
 for i in $list; do
-	ip=`echo $i | awk -F= '{print $1}'`
-	num=`echo $i | awk -F= '{print $2}'`
-	# 失败超过3次，加入黑名单
-	if [[ $num -gt 3 ]]; then
-		grep $ip /etc/hosts.deny &> /dev/null
-		if [[ $? -gt 0 ]]; then
-			echo "sshd:$ip" >> /etc/hosts.deny
-		fi
-		# 将每个ip尝试次数保存在banIp.log中
-		grep $ip /opt/scripts/banIp.log &> /dev/null
-		if [[ $? -gt 0 ]]; then
-			echo "$ip=$num" >> /opt/scripts/banIp.log 
-		else
-			sed -n 's/$ip=\d+/$ip=$num/' /opt/scripts/banIp.log
-		fi
-	fi
+  if ! [[ "$i" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} ]]; then
+    continue
+  fi
+  ip=$(echo "$i" | awk -F= '{print $1}')
+  num=$(echo "$i" | awk -F= '{print $2}')
+  # 失败超过3次，加入黑名单
+  if [[ $num -gt 3 ]]; then
+    grep "$ip" /etc/hosts.deny &>/dev/null
+    if [[ $? -gt 0 ]]; then
+      echo "ALL:$ip" >>/etc/hosts.deny
+      iptables -I INPUT -s "$ip/32" -j DROP
+    fi
+    # 将每个ip尝试次数保存在banIp.log中
+    grep "$ip" /opt/scripts/banIp.log &>/dev/null
+    if [[ $? -gt 0 ]]; then
+      echo "$ip=$num" >>/opt/scripts/banIp.log
+    else
+      sed -n 's/$ip=\d+/$ip=$num/' /opt/scripts/banIp.log
+    fi
+  fi
 done
 
 cp /dev/null /tmp/black
