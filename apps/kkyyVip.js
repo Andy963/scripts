343911be63fafe4b9d333433314bd2d45764ad0b
Andy963 <Andy963@users.noreply.github.com>
2024-02-05 14:57:18 +0800

    update: new method

--- apps/kkyyVip.js
+++ apps/kkyyVip.js
@@ -6,18 +6,21 @@
  * @FilePath: \undefinedd:\PycharmProjects\own\scripts\apps\kkyyVip.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-const is_vip = 'var is_vip=0';
-const is_vip_str = 'var is_vip=1'
-const end_time = `var end_time=0`;
-const end_time_str = `var end_time=${getExpireStr()}`;
-const expire_time = 'var expire_time=0';
-const expire_time_str = `var expire_time=${getExpireStr()}`;
-const body = $response.body.replace(is_vip, is_vip_str).replace(expire_time, expire_time_str).replace(end_time, end_time_str);
-$done({body: body})
-
-//主程序执行入口
 
 function getExpireStr() {
     let d = new Date();
     return d.setMonth(d.getMonth() + 6 + 1);
 }
+
+let body = $response.body;
+let obj = JSON.parse(body);
+if (obj?.Data?.is_vip === 0) {
+    obj['Data']['is_vip'] = 1;
+    obj['Data']['expire_time'] = getExpireStr();
+    obj['Data']['end_time'] = getExpireStr();
+}
+body = JSON.stringify(obj);
+
+console.log(body);
+
+$done(body);
